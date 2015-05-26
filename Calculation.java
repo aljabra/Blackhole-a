b075f830fdd9fe8c717c62379e4660ba9f70e3af
Jay Patel <jpatel5@students.d125.org>
2015-05-26 09:22:45 -0500

    A

---
+++ Calculation.java
@@ -0,0 +1,33 @@
+package a;
+
+import java.util.Scanner;
+/**
+ * Write a description of class Calculation here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Calculation
+{
+    public static void main(String s[])
+    {
+        System.out.println("Welcome to the Blackhole");
+        System.out.println("Enter the time constant");
+        Scanner a = new Scanner(System.in);
+        double constant = a.nextDouble();
+        System.out.println("Enter the initial distance");
+        double x = a.nextDouble();
+        System.out.println("Enter the product of charges in nanoColumbs");
+        double q = a.nextDouble();
+        System.out.println("Enter how many time constant");
+        int n = a.nextInt();
+        Black_hole b = new Black_hole(constant,x,q);
+        for(int i = 1; i <= n; i++)
+        {
+            b.change();
+            System.out.println("Acceleration after " + i + " time constant is " + b.getAcceleration());
+            System.out.println("Velocity after " + i + " time constant is " + b.getVelocity());
+            System.out.println("Displacement after " + i + " time constant is " + b.getDisplacement());
+        }
+    }
+}
